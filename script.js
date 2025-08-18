243f7b2d780dec050a969467719c53f835aefe9b
My-Hospital <viveksunilchavhan@gmail.com>
2025-08-18 22:05:18 +0530

    add animation in login and signup form

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
+
+
 function xpage () {
 
-let hideform = document.getElementById("log"), hidesign = document.getElementById("sign");
+    let logForm = document.getElementById("log"), signForm = document.getElementById("sign");
 
-if (hidesign.hidden){
-    hideform.hidden = true;
-    hidesign.hidden = false;
+    if(!signForm.hasAttribute("hidden")){
+        signForm.setAttribute("hidden",true);
+        logForm.removeAttribute("hidden");
+    }else {
+        logForm.setAttribute("hidden",true);
+        signForm.removeAttribute("hidden");
+    }
+
 }
-else{
-    hideform.hidden = false;
-    hidesign.hidden = true;
-}   
-}
+
+
+
+document.getElementById("log-btn").addEventListener("click",()=>{
+    alert("Login Successfully.....");
+})
+
+document.getElementById("sign-btn").addEventListener("click",()=>{
+    alert("Account Create Successfully.....")
+})
+
+let fill = document.querySelector("input");
+    if(!fill.value){
+        alert("Please fill details");
+        (!fill.value).focus();
+        
+    }
